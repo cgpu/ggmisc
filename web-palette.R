6fe2077bc0efb60d827aeb61cc921b8556190afb
cgpu <38183826+cgpu@users.noreply.github.com>
2019-05-05 19:09:41 +0100

    Refactoring to create hex extractor function

--- web-palette.R
+++ web-palette.R
@@ -1,23 +1,28 @@
-webPalette <- function(){
-my_color_palette = data.frame(hex        = c('#0198F5' ,  # 1. cta-blue 
-                                             '#FA5E5B',   # 2. watermelon-fail
-                                             '#FEC334',   # 3. notification-yellow
-                                             '#31C052',   # 4. success-green
-                                             '#DFE6F4',   # 5. grey-light
-                                             '#A9BACA', 	# 6. grey-medium 	
-                                             '#4A637B',   # 7. grey-dark
-                                             '#000000',   # 8. black-treacle
-                                             '#FFFFFF'    # 9. unicorn-white
-                                            ),
-                              colorname = c( "cta-blue "          ,
-                                             "watermelon-fail"    ,
-                                             "notification-yellow",
-                                             "success-green"      ,
-                                             "grey-light"         ,
-                                             "grey-medium "       ,
-                                             "grey-dark"          ,
-                                             "black-treacle"      ,
-                                             "unicorn-white"  )
-                              )
-  return(my_color_palette)
+webPalette <- function(...){
+      
+      # Totally found it here: https://drsimonj.svbtle.com/creating-corporate-colour-palettes-for-ggplot2
+  
+      # Set colors
+      my_color_palette  = c( `cta blue`            = '#0198F5' ,  # 1. cta-blue 
+                             `watermelon fail` .   = '#FA5E5B',   # 2. watermelon-fail
+                             `notification yellow` = '#FEC334',   # 3. notification-yellow
+                             `success green`       = '#31C052',   # 4. success-green
+                             `grey light`          = '#DFE6F4',   # 5. grey-light
+                             `grey medium`         = '#A9BACA', 	# 6. grey-medium 	
+                             `grey dark`           = '#4A637B',   # 7. grey-dark
+                             `black-treacle`       = '#000000',   # 8. black-treacle
+                             `unicorn-white`       = '#FFFFFF' )  # 9. unicorn-white                                           ),                              )
+
+
+      # Roxygen comments: 
+      #' Function to extract my_color_palette colors as hex codes
+      #'
+      #' @param ... Character names of my_color_palette 
+      #'
+        cols <- c(...)
+
+        if (is.null(cols))
+          return (my_color_palette)
+
+        my_color_palette[cols]
 }
