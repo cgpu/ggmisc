0e86de36033db894efadf0576cdadd0f01bee962
cgpu <38183826+cgpu@users.noreply.github.com>
2019-06-01 23:36:15 +0100

    Update HUNTpalette.R

--- HUNTpalette.R
+++ HUNTpalette.R
@@ -21,7 +21,7 @@ my_color_palette  =  c( `pink cranberry`      = '#da627d',
 #'
 #' @param ... Character names of my_color_palette 
 #'
-HUNTpalette <- function(...) {
+HUNTpalette  <- function(...) {
   cols <- c(...)
 
   if (is.null(cols))
